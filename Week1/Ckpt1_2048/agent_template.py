969356f71e31c665c55cb08362638dd743d6cb88
Kunind27 <kunind444@gmail.com>
2021-04-12 17:01:16 +0530

    Fixed the Infinite Loop bug and documented the code

--- Week1/Ckpt1_2048/agent_template.py
+++ Week1/Ckpt1_2048/agent_template.py
@@ -8,29 +8,29 @@ and the value of each Node is the increase in the score from the previous state 
 """
 
 def expecti_minimax(board_state, is_current, depth_rem):
-    # Return current score if max depth of tree reached
+    # Return current score if max depth of tree reached or no other moves possible
     if depth_rem == 0 or not board_state.check_state():
-        return board_state.score,0
+        return board_state.score, 0
     else:
         # Current Board State calc. score of each move up to max_depth, chooses that move which Maximises Score
         # Returns Best Score of the Move as well as The Move itself
         if is_current:
             bs1 = copy.deepcopy(board_state)
             bs1.update('L')
-            left = expecti_minimax(bs1, False, depth_rem)[0] if board_state.check_move('L') \
-                else -1
+            left = expecti_minimax(bs1, False, depth_rem)[0] if board_state.check_move('L') else -1
+
             bs2 = copy.deepcopy(board_state)
             bs2.update('R')
-            right = expecti_minimax(bs2, False, depth_rem)[0] if board_state.check_move('R') \
-                else -1
+            right = expecti_minimax(bs2, False, depth_rem)[0] if board_state.check_move('R') else -1
+
             bs3 = copy.deepcopy(board_state)
             bs3.update('U')
-            up = expecti_minimax(bs3, False, depth_rem)[0] if board_state.check_move('U') \
-                else -1
+            up = expecti_minimax(bs3, False, depth_rem)[0] if board_state.check_move('U') else -1
+
             bs4 = copy.deepcopy(board_state)
             bs4.update('D')
-            down = expecti_minimax(bs4, False, depth_rem)[0] if board_state.check_move('D') \
-                else -1
+            down = expecti_minimax(bs4, False, depth_rem)[0] if board_state.check_move('D') else -1
+
             return max(left, right, down, up), np.argmax(np.array([left, right, up, down]))
 
         # If it is not the current Node then it takes the Expected Value of Scores of All Possible Moves L,R,U,D
@@ -39,27 +39,29 @@ def expecti_minimax(board_state, is_current, depth_rem):
         else:
             bs1 = copy.deepcopy(board_state)
             bs1.update('L')
-            left = expecti_minimax(bs1, True, depth_rem - 1)[0] if board_state.check_move('L') \
-                else -1
+            left = expecti_minimax(bs1, True, depth_rem - 1)[0] if board_state.check_move('L') else -1
+
             bs2 = copy.deepcopy(board_state)
             bs2.update('R')
-            right = expecti_minimax(bs2, True, depth_rem - 1)[0] if board_state.check_move('R') \
-                else -1
+            right = expecti_minimax(bs2, True, depth_rem - 1)[0] if board_state.check_move('R') else -1
+
             bs3 = copy.deepcopy(board_state)
             bs3.update('U')
-            up = expecti_minimax(bs3, True, depth_rem - 1)[0] if board_state.check_move('U') \
-                else -1
+            up = expecti_minimax(bs3, True, depth_rem - 1)[0] if board_state.check_move('U') else -1
+
             bs4 = copy.deepcopy(board_state)
             bs4.update('D')
-            down = expecti_minimax(bs4, True, depth_rem - 1)[0] if board_state.check_move('D') \
-                else -1
-            ctr = 0
-            tot = 0
+            down = expecti_minimax(bs4, True, depth_rem - 1)[0] if board_state.check_move('D') else -1
+
+            # To return the score: Finds all allowed moves, performs each one; takes an average of the scores after each
+            # allowed move and returns it, if zero allowed moves, returns score of the current board.
+            counter = 0
+            total = 0
             for mv in [left,right,up,down]:
                 if(mv>0):
-                    ctr += 1
-                    tot += mv
-            return tot/ctr,0
+                    counter += 1
+                    total += mv
+            return (total/counter,0) if counter != 0 else (board_state.score,0)
 
 def agent(game_board, max_depth=2):
     """
