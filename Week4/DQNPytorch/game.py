7fead888f984f5fb1febd5e015667d74366d0d7f
Kunind Sahu <kunind444@gmail.com>
2021-07-18 20:59:54 +0530

    Added Code for DQN for 2048 game

---
+++ Week4/DQNPytorch/game.py
@@ -0,0 +1,28 @@
+"""
+print state & current score
+input user 
+move(move)
+check()
+valid move hai->continue or print final score
+"""
+from board import board
+
+game_board = board()
+
+key_to_move = {'w':'U','a':'L','s':'D','d':'R'}
+
+while(True):
+    print(game_board)
+    key = input()     
+    move = key_to_move[key]
+    if(game_board.check_move(move)):
+        game_board.update(move)
+        game_board.spawn()
+    else:
+        print("Invalid Move!!!\n")
+        continue
+    if(not game_board.check_state()):
+        break
+    
+print(game_board)
+print("Game Over !!!")
